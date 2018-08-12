6f024bc36f916ca02f2f63ccc90bb1e7177919be
Joshua Haan <joshua.haan@ymail.com>
2018-08-11 19:40:35 -0700

    adds rudimentary user creation

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ end
 
 
 gem 'rails', '~> 5.1.6'
+gem 'bcrypt'
 gem 'puma', '~> 3.7'
 gem 'sass-rails', '~> 5.0'
 gem 'bootstrap-sass'
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,56 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "John", email: "john@example.com",
+                     password: 'supersecure', password_confirmation: 'supersecure')
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "should be invalid if name exceeds 50 char" do
+    @user.name = "x" * 51
+    assert_not @user.valid?
+  end
+
+  test 'should be invalid if name missing' do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test 'should be invalid if email missing' do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "should be invalid if email exceeds 255 char" do
+    @user.email = "x" * 256
+    assert_not @user.valid?
+  end
+
+  test "should be invalid if email is not unique" do
+    dup_user = @user.dup
+    @user.save
+    assert_not dup_user.valid?
+  end
+
+  test "should save email downcase" do
+    mixed_case = "wHat.examPle.cOm"
+    @user.email = mixed_case
+    @user.save
+    @user.reload
+    assert_equal mixed_case.downcase, @user.email
+  end
+
+  test 'should have password' do
+    @user.password = @user.password_confirmation = " " * 8
+    assert_not @user.valid?
+  end
+
+  test 'should have minimum length' do
+    @user.password = @user.password_confirmation = 'x' * 7
+    assert_not @user.valid?
+  end
 end
